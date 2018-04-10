532d642b6f67bb057012c6dbb9cac69744fac8ee
dilushaj <dilusha.15@cse.mrt.ac.lk>
2018-04-10 23:34:02 +0530

    broadcasting

--- Driver/broadcast.php
+++ Driver/broadcast.php
@@ -1,9 +1,24 @@
 <?php
 include 'dbcon.php';
+include "DbAccess.php";
+$dbAccess=new DbAccess();
 $sql1="Select deviceId from Device";
-$result = $conn1->query($sql);
+$sql2="select * from animal where globalStatus='not broadcasted'";
+$sql3="Update animal set globalStatus='broadcasted' where globalStatus='not broadcasted' ";
+$conn1->busyTimeout(5000);
+$conn1->exec('PRAGMA journal_mode = wal;');
+$conn1->query("begin transaction");
+$result1 = $conn1->query($sql1);
 $deviceId="";
-while ($row = $result->fetchArray(SQLITE3_ASSOC)){
+while ($row = $result1->fetchArray(SQLITE3_ASSOC)){
     $deviceId=$row['deviceId'] . '<br/>';
 }
-$sql2="select * from animal where globalStatus='not broadcasted'";
+
+$result2 = $conn1->query($sql2);
+while ($row = $result2->fetchArray(SQLITE3_ASSOC)){
+
+    $dbAccess->saveToWebServer($row['animalName'],$row['longitude'],$row['latitude'],$deviceId,$row['time']);
+}
+$conn1->query($sql3);
+$conn1->query("commit");
+$conn1->close();
